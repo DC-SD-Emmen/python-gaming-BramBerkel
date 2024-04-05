7ce6d50eebf16f7bfb6b27e195388a9e67de7efa
BramBerkel <144883590+BramBerkel@users.noreply.github.com>
2024-04-05 12:41:06 +0200

    Added proper display

--- Battleship/main.py
+++ Battleship/main.py
@@ -1,8 +1,9 @@
 import pygame
+import random
 
 pygame.init()
 
-window_size = (800, 600)
+window_size = (750, 750)
 window = pygame.display.set_mode(window_size)
 pygame.display.set_caption("My Pygame Window")
 
@@ -15,9 +16,19 @@ while running:
 
     # Draw shapes
     window.fill((255, 255, 255))
-    pygame.draw.circle(window, (255, 0, 0), (150, 200), 50)
-    pygame.draw.rect(window, (0, 200, 0), (100, 300, 300, 200))
-    pygame.draw.line(window, (0, 0, 100), (100, 100), (700, 500), 5)
+    # pygame.draw.circle(window, (255, 0, 0), (150, 200), 50)
+    # pygame.draw.rect(window, (0, 200, 0), (100, 300, 300, 200))
+    # pygame.draw.line(window, (0, 0, 100), (100, 100), (700, 500), 5)
+
+    pygame.draw.rect(window, (0, 0, 0), (97, 97, 533, 533))
+    for length in range(10):
+        vertdistance = 100+53*length
+        for width in range(10):
+            hordistance = 100+53*width
+            pygame.draw.rect(window, (255, 255, 255), (hordistance, vertdistance, 50, 50))
+            width += 1
+        length += 1
+        width = 0
 
     pygame.display.flip()
 pygame.quit()
